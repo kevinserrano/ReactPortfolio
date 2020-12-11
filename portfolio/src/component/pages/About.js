8663eb37e3fdfef4550fb64919dafb7e827abb75
kevinserrano <66225834+kevinserrano@users.noreply.github.com>
2020-12-10 18:04:07 -0600

    setting up react

---
+++ portfolio/src/component/pages/About.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const About = () => {
+    return (
+    <div>
+        <h1 id="aboutMeTitle">About Me</h1>
+    <section id="aboutMe">
+        <p>
+            I was born Nov. 30th 1984 in San Marcos, TX. I currently reside in San Antonio, TX and work
+            in
+            the
+            oil field
+            industry. The company I work for is based out of Oklahoma City and we provide many forms of
+            communication
+            to companies out in the oil field. I have two beautiful and amazing children and to top it
+            off
+            an
+            equally
+            amazing and beautiful wife.
+        </p>
+        <p>
+            I am also currently a part-time student at UTSA and I am studying on becoming a web
+            developer.
+            My
+            drive in
+            life is my family and the need and want to provide an amazing life for them. I am
+            extremely close to my father, Al Serrano who is now retired from a 37yr career with Wal-Mart
+            where
+            he
+            reached
+            Regional Vice President status. I would have to credit him for instilling the values of hard
+            work
+            and
+            family.
+        </p>
+    </section>
+    </div>
+    )
+}
+
+export default About;
---
+++ portfolio/src/component/pages/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => (
+  <div>
+   <h1>Kevin Serrano</h1>
+  </div>
+);
+
+export default Home;
